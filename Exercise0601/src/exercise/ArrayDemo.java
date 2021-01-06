0db2840c9a40246b9b81f8fa9c77cf8747d2f4cd
Luong Bang <39915879+BangDz69@users.noreply.github.com>
2021-01-06 18:57:07 +0700

    Add files via upload

---
+++ Exercise0601/src/exercise/ArrayDemo.java
@@ -0,0 +1,26 @@
+/*
+ * @author LuongBang16
+ * @date Jan 6, 2021
+ * @version 1.0
+ */
+package exercise;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		//Declare an array
+		int[] arrayInt = {1, 2, 3, 4, 5};
+		
+		//set value for each element of array
+//		arrayInt[0] = 1;
+//		arrayInt[1] = 2;
+//		arrayInt[2] = 3;
+//		arrayInt[3] = 4;
+//		arrayInt[4] = 5;
+		
+		//get array length
+		System.out.println("Array length: " + arrayInt.length);
+		System.out.println("Element at index 3 is: " + arrayInt[3]);
+	}
+
+}
